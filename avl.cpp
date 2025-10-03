12c0709980373d16a756c7fe85e526f5265634a0
falo <faliaraoz@gmail.com>
2025-10-02 23:04:58 -0500

    AVL implmentation

---
+++ avl.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node* left;
+    node* right;
+    int bal; //factor de balance
+    int count;
+
+    node(int x):data(x), left(nullptr), right(nullptr), bal(0), count(1) {}
+};
+
+
+//LL (A = +2; B = +1) RR (A = -2; B = -1)
+//LR (A = +2; B = -1) RL (A = -2; B = +1)
+class avl{
+private:
+    node* root = nullptr;
+public:
+    void LL(node* &a){      //a = +2; b = +1
+        node* b = a->left;
+        a->left = b->right;
+        b->right = a;
+
+        a->bal = 0;
+        b->bal = 0;
+        a = b; //nuevo root
+    }
+    void RR(node* &a){      //a = -2; b = -1
+        node* b = a->right;
+        a->right = b->left;
+        b->left = a;
+
+        a->bal = 0;
+        b->bal = 0;
+        a = b; //nuevo root
+    }
+    void LR(node* &a){      //a = +2; b = -1
+        node* b = a->left;
+        node* c = b->right;
+
+        b->right = c->left;
+        c->left = b;
+        a->left = c->right;
+        c->right = a;
+
+        if(c->bal == 1){ //c estaba a la izquierda
+            a->bal = -1;
+            b->bal = 0;
+        }
+        else if(c->bal == 0){ //c estaba en 0
+            a->bal = 0;
+            b->bal = 0;
+        }
+        else{ //c estaba a la derecha
+            a->bal = 0;
+            b->bal = 1;
+        }
+        c->bal = 0;
+        a = c;//nuevo root
+    }
+    void RL(node* &a){      //a = -2; b = +1
+        node* b = a->right;
+        node* c = b->left;
+
+        b->left = c->right;
+        c->right = b;
+        a->right = c->left;
+        c->left = a;
+
+        if(c->bal == 1){ //c estaba a la izquierda
+            a->bal = 0;
+            b->bal = -1;
+        }
+        else if(c->bal == 0){ //c estaba en 0
+            a->bal = 0;
+            b->bal = 0;
+        }
+        else{ //c estaba a la derecha
+            a->bal = 1;
+            b->bal = 0;
+        }
+        c->bal = 0;
+        a = c;//nuevo root
+    }
+
+    
+
+
+    
+
+};
+
+
+
+
